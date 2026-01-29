290a7f47718835a6c61a47c1a75785a94675ef64
Simon Støvring <mail@simonbs.dk>
2026-01-29 13:59:52 +0100

    Fix SHA256 checksum

--- Formula/wut.rb
+++ Formula/wut.rb
@@ -2,7 +2,7 @@ class Wut < Formula
   desc "Git worktree manager that keeps worktrees out of your repo"
   homepage "https://github.com/simonbs/wut"
   url "https://github.com/simonbs/wut/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "88a67f61a59bb0d7733ba2328b0b0a575a821330389171838a371f26dd6a7809"
 
   depends_on "go" => :build
 
